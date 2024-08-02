aa1efb0ed0be6ceab19f0ae777d02c92415d799f
m00nsh <mo3ov.877@gmail.com>
2024-08-02 09:53:18 +0900

    追加

---
+++ kadai/src/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+  const $pageTop = $("#pagetop");
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 1) {
+      $pageTop.fadeIn(300).css("display", "flex");
+    } else {
+      $pageTop.fadeOut(300);
+    }
+  });
+
+  $pageTop.click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 300);
+  });
+});
