e02093a3e927c615507c732351ddf36609af5a23
m00nsh <mo3ov.877@gmail.com>
2024-08-06 20:22:23 +0900

    変更

--- kadai/src/js/dist/script.dev.js
+++ kadai/src/js/dist/script.dev.js
@@ -19,4 +19,18 @@ $(function () {
     }, 500);
     return false;
   });
+}); // アコーディオンメニュー
+
+$(function () {
+  // .js-accordion_titleをクリックすると
+  $(".accordion_title--js").click(function () {
+    // クリックした次の要素を展開
+    $(this).next(".accordion__inner--js").slideToggle(); // 展開するときjs-accordion_titleクラスにopenクラスを追加してアイコンを回転
+
+    $(this).toggleClass("open"); // クリックされた.js-accordion_title以外の.js-accordion_titleに隣接する.js-accordion_innerを閉じる処理
+
+    $(".accordion__ttl--js").not($(this)).next(".js-accordion_inner").slideUp(); // 閉じられた他の.js-accordion_title要素に追加したopenクラスを外しアイコンを元に戻す処理
+
+    $(".accordion__ttl--js").not($(this)).removeClass("open");
+  });
 });
