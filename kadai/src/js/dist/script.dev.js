b740884089c59c8246390fd9e6a2369f2f2999f6
m00nsh <mo3ov.877@gmail.com>
2024-08-06 15:32:32 +0900

    追加

--- kadai/src/js/dist/script.dev.js
+++ kadai/src/js/dist/script.dev.js
@@ -1,25 +1,22 @@
+"use strict";
+
 $(function () {
   // ボタン消す
-  $(".pagetop").hide();
-
-  // スクロールしたらボタン現れる
+  $(".pagetop").hide(); // スクロールしたらボタン現れる
   // 上に戻ったらボタン消える
+
   $(window).scroll(function () {
     if ($(this).scrollTop() > 60) {
       $(".pagetop").fadeIn();
     } else {
       $(".pagetop").fadeOut();
     }
-  });
+  }); // ボタン押されたら
 
-  // ボタン押されたら
   $(".pagetop").click(function () {
-    $("body, html").animate(
-      {
-        scrollTop: 0,
-      },
-      500
-    );
+    $("body, html").animate({
+      scrollTop: 0
+    }, 500);
     return false;
   });
 });
